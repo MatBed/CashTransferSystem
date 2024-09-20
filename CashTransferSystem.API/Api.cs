103fbef838c6dca460ee473c2e28a87414b20ac7
MatBed <mateuszbedla@gmail.com>
2024-09-20 11:17:28 +0200

    clean files

--- CashTransferSystem.API/Api.cs
+++ CashTransferSystem.API/Api.cs
@@ -5,43 +5,42 @@ using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CashTransferSystem.API
+namespace CashTransferSystem.API;
+
+public static class Api
 {
-    public static class Api
+    public static void ConfigureApi(this WebApplication app)
     {
-        public static void ConfigureApi(this WebApplication app)
+        app.MapGet("/transfers", GetCashTransfers).WithSummary("Endpoint for get collection with cash transfers");
+        app.MapGet("/transferTypes", GetTransferTypes).WithSummary("Endpoint for get transfer types");
+        app.MapPost("/transfers", CreateCashTransfers).WithSummary("Endpoint for create cash transfer");
+    }
+
+    private static async Task<IResult> GetCashTransfers([FromServices] IMediator mediator)
+    {
+        var transfers = await mediator.Send(new GetTransfersQuery());
+        return Results.Ok(transfers);
+    }
+
+    private static async Task<IResult> GetTransferTypes([FromServices] IMediator mediator)
+    {
+        var transfers = await mediator.Send(new GetTransferTypesQuery());
+        return Results.Ok(transfers);
+    }
+
+    private static async Task<IResult> CreateCashTransfers(
+        IValidator<CreateTransferCommand> validator,
+        [FromBody] CreateTransferCommand command,
+        [FromServices] IMediator mediator)
+    {
+        var validationResult = await validator.ValidateAsync(command);
+
+        if (!validationResult.IsValid)
         {
-            app.MapGet("/transfers", GetCashTransfers).WithSummary("Endpoint for get collection with cash transfers");
-            app.MapGet("/transferTypes", GetTransferTypes).WithSummary("Endpoint for get transfer types");
-            app.MapPost("/transfers", CreateCashTransfers).WithSummary("Endpoint for create cash transfer");
+            return Results.BadRequest(validationResult.Errors);
         }
 
-        private static async Task<IResult> GetCashTransfers([FromServices] IMediator mediator)
-        {
-            var transfers = await mediator.Send(new GetTransfersQuery());
-            return Results.Ok(transfers);
-        }
-
-        private static async Task<IResult> GetTransferTypes([FromServices] IMediator mediator)
-        {
-            var transfers = await mediator.Send(new GetTransferTypesQuery());
-            return Results.Ok(transfers);
-        }
-
-        private static async Task<IResult> CreateCashTransfers(
-            IValidator<CreateTransferCommand> validator,
-            [FromBody] CreateTransferCommand command,
-            [FromServices] IMediator mediator)
-        {
-            var validationResult = await validator.ValidateAsync(command);
-
-            if (!validationResult.IsValid)
-            {
-                return Results.BadRequest(validationResult.Errors);
-            }
-
-            var success = await mediator.Send(command);
-            return Results.Ok(new { Success = success });
-        }
+        var success = await mediator.Send(command);
+        return Results.Ok(new { Success = success });
     }
 }
---
+++ CashTransferSystem.UI/ViewModels/TransferViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace CashTransferSystem.UI.ViewModels;
+
+public record TransferViewModel(string SenderFirstName,
+    string SenderLastName,
+    string SenderPesel,
+    string ReceiverAccountNumber,
+    string ReceiverName,
+    string ReceiverNip,
+    string ReceiverRegon,
+    string ReceiverAddress,
+    decimal Amount,
+    string TransferType,
+    string Status);
