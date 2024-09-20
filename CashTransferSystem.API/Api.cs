2880c221c920740a0255ccc1232776f77802986c
MatBed <mateuszbedla@gmail.com>
2024-09-20 11:04:37 +0200

    add fluent validation for api

--- CashTransferSystem.API/Api.cs
+++ CashTransferSystem.API/Api.cs
@@ -1,6 +1,7 @@
 ﻿using CashTransferSystem.Application.Features.Transfers.Commands.CreateTransfer;
 using CashTransferSystem.Application.Features.Transfers.Queries.GetTransfers;
 using CashTransferSystem.Application.Features.Transfers.Queries.GetTransferTypes;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +12,14 @@ namespace CashTransferSystem.API
         public static void ConfigureApi(this WebApplication app)
         {
             app.MapGet("/transfers", GetCashTransfers).WithSummary("Endpoint for get collection with cash transfers");
-            app.MapPost("/transfers", CreateCashTransfers).WithSummary("Endpoint for create cash transfer");
             app.MapGet("/transferTypes", GetTransferTypes).WithSummary("Endpoint for get transfer types");
+            app.MapPost("/transfers", CreateCashTransfers).WithSummary("Endpoint for create cash transfer");
+        }
+
+        private static async Task<IResult> GetCashTransfers([FromServices] IMediator mediator)
+        {
+            var transfers = await mediator.Send(new GetTransfersQuery());
+            return Results.Ok(transfers);
         }
 
         private static async Task<IResult> GetTransferTypes([FromServices] IMediator mediator)
@@ -22,17 +29,19 @@ namespace CashTransferSystem.API
         }
 
         private static async Task<IResult> CreateCashTransfers(
+            IValidator<CreateTransferCommand> validator,
             [FromBody] CreateTransferCommand command,
             [FromServices] IMediator mediator)
         {
+            var validationResult = await validator.ValidateAsync(command);
+
+            if (!validationResult.IsValid)
+            {
+                return Results.BadRequest(validationResult.Errors);
+            }
+
             var success = await mediator.Send(command);
             return Results.Ok(new { Success = success });
-        }
-
-        private static async Task<IResult> GetCashTransfers([FromServices] IMediator mediator)
-        {
-            var transfers = await mediator.Send(new GetTransfersQuery());
-            return Results.Ok(transfers);
         }
     }
 }
---
+++ CashTransferSystem.Application/Features/Transfers/Commands/CreateTransfer/CreateTransferCommandValidator.cs
@@ -0,0 +1,66 @@
+﻿using CashTransferSystem.Core.Enums;
+using FluentValidation;
+
+namespace CashTransferSystem.Application.Features.Transfers.Commands.CreateTransfer;
+
+public class CreateTransferCommandValidator : AbstractValidator<CreateTransferCommand>
+{
+    private const int PESEL_LENGTH = 11;
+    private const int NIP_LENGTH = 10;
+    private const int REGON_LENGTH = 9;
+
+    public CreateTransferCommandValidator()
+    {
+        RuleFor(x => x.SenderFirstName)
+            .NotNull().WithMessage("Sender first name is required.")
+            .NotEmpty().WithMessage("Sender first name is required.");
+
+        RuleFor(x => x.SenderLastName)
+            .NotNull().WithMessage("Sender last name is required.")
+            .NotEmpty().WithMessage("Sender last name is required.");
+
+        RuleFor(x => x.SenderPesel)
+            .NotNull().WithMessage("Sender pesel is required.")
+            .NotEmpty().WithMessage("Sender pesel is required.")
+            .Length(PESEL_LENGTH).WithMessage($"Sender pesel must have {PESEL_LENGTH} digits.")
+            .Matches(@"^\d+$").WithMessage("Sender pesel must contain only digits.");
+
+        RuleFor(x => x.ReceiverAccountNumber)
+            .NotNull().WithMessage("Receiver account number is required.")
+            .NotEmpty().WithMessage("Receiver account number is required.")
+            .Length(26).WithMessage("Receiver account number must have exactly 26 digits.")
+            .Matches(@"^\d+$").WithMessage("Receiver account number must contain only digits.");
+
+        RuleFor(x => x.ReceiverName)
+            .NotNull().WithMessage("Receiver name is required.")
+            .NotEmpty().WithMessage("Receiver name is required.");
+
+        RuleFor(x => x.ReceiverNip)
+            .NotNull().WithMessage("Receiver NIP is required.")
+            .NotEmpty().WithMessage("Receiver NIP is required.")
+            .Length(NIP_LENGTH).WithMessage($"Receiver NIP must have {NIP_LENGTH} digits.")
+            .Matches(@"^\d+$").WithMessage("Receiver NIP must contain only digits.");
+
+        RuleFor(x => x.ReceiverRegon)
+            .NotNull().WithMessage("Receiver REGON is required.")
+            .NotEmpty().WithMessage("Receiver REGON is required.")
+            .Length(REGON_LENGTH).WithMessage($"Receiver REGON must have {REGON_LENGTH} digits.")
+            .Matches(@"^\d+$").WithMessage("Receiver REGON must contain only digits.");
+
+        RuleFor(x => x.ReceiverAddress)
+            .NotNull().WithMessage("Receiver address is required.")
+            .NotEmpty().WithMessage("Receiver address is required.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0).WithMessage("Amount must be greater than zero.");
+
+        RuleFor(x => x.TransferTypeId)
+            .Must(BeAValidEnumValue)
+            .WithMessage("Transfer type must be a valid value from the enumeration.");
+    }
+
+    private bool BeAValidEnumValue(int transferTypeId)
+    {
+        return Enum.IsDefined(typeof(TransferType), transferTypeId);
+    }
+}
